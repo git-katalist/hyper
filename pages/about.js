87c09ab9d0de42231d7d89abbed901e4a83850b4
tsots <mosalatsotetsi@gmail.com>
2022-08-01 03:28:00 +0200

    add dynamic logo navbar image

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
-import React from "react";
+import styles from "../styles/Home.module.css";
 
 const about = () => {
-  return <div>about</div>;
+  return <div className={styles.container}>about</div>;
 };
 
 export default about;
